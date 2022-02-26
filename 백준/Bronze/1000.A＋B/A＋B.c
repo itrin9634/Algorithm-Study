2de53455db2f2f5ab1a5e0dc29d7f407ce58e5ff
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-02-26 14:24:29 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/1000.A＋B/A＋B.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{ 
+    int num1;
+    int num2;
+    int sum;
+
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+
+    sum = num1+num2;
+    printf("%d", sum);
+    return 0;
+}
