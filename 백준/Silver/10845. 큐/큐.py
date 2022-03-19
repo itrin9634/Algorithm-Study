3da9dffcef5b5c6ff88e88a89dbdfa3926b90e8b
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-03-19 20:50:40 +0900

    [Silver IV] Title: 큐, Time: 172 ms, Memory: 127436 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,30 @@
+import sys
+n = int(sys.stdin.readline())
+queue = []
+
+for i in range(n):
+    s = sys.stdin.readline().split()
+    if s[0] == "push":  # push
+        queue.append(s[1])
+    elif s[0] == "pop":
+        if len(queue) > 0:
+            print(queue.pop(0))
+        else:
+            print(-1)
+    elif s[0] == "size":
+        print(len(queue))
+    elif s[0] == "empty":
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    elif s[0] == "front":
+        if len(queue) > 0:
+            print(queue[0])
+        else:
+            print(-1)
+    elif s[0] == "back":
+        if len(queue) > 0:
+            print(queue[-1])
+        else:
+            print(-1)
