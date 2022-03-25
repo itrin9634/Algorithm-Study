19f532952090e02463507fd1321fee46b8418047
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-03-25 13:47:22 +0900

    [Silver III] Title: 소수 구하기, Time: 952 ms, Memory: 127604 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,15 @@
+m, n = map(int, input().split())
+
+
+def isPrime(x):
+    if x == 1:
+        return False
+    for i in range(2, int(x ** 0.5) + 1):
+        if x % i == 0:
+            return False
+    return True
+
+
+for i in range(m, n + 1):
+    if isPrime(i):
+        print(i)
