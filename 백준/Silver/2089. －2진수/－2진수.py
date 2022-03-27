856ce1633f8b699fc8b60bf8ec8c25a09d5c6273
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-03-27 20:31:43 +0900

    [Silver IV] Title: -2진수, Time: 108 ms, Memory: 123316 KB -BaekjoonHub

--- 백준/Silver/2089. －2진수/－2진수.py
+++ 백준/Silver/2089. －2진수/－2진수.py
@@ -1,14 +1,13 @@
-import sys
-N = int(sys.stdin.readline())
-if not N:
-    sys.stdout.write('0')
-    exit()
+n = int(input())
 res = ''
-while N:
-    if N % -2:
-        res = '1' + res
-        N = N // -2 + 1
-    else:
-        res = '0' + res
-        N //= -2
-sys.stdout.write(res)
+if n == 0:
+    res = 0
+else:
+    while n:
+        if n % (-2):
+            res = '1' + res
+            n = n // -2 + 1
+        else:
+            res = '0' + res
+            n //= -2
+print(res)
