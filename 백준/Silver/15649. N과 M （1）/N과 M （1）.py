b3890ae4d4917a50a45664ce22cb057aeefd4ac0
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-04-14 23:53:35 +0900

    [Silver III] Title: N과 M (1), Time: 200 ms, Memory: 116904 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,10 +1,18 @@
-from itertools import permutations
 n, m = map(int, input().split())
-nums = [i for i in range(1, n+1)]
-arr = list(permutations(nums, m))
-arr.sort()
-for i in range(len(arr)):
-    for j in arr[i]:
-        print(j, end=' ')
-    if i != len(arr) -1:
-        print()
+s = []
+
+
+def f():
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
+
+    for i in range(1, n+1):
+        if i in s:
+            continue
+        s.append(i)
+        f()
+        s.pop()
+
+
+f()
