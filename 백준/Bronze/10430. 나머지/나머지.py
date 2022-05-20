66ed2d604646bb7cc9267382039838a8890c3afb
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-05-20 15:19:45 +0900

    [Bronze V] Title: 나머지, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.py
+++ 백준/Bronze/10430. 나머지/나머지.py
@@ -1,6 +1,5 @@
-a, b, c = map(int, input().split())
-
-print((a + b) % c)
-print(((a % c) + (b % c)) % c)
-print((a * b) % c)
-print(((a % c) * (b % c)) % c)
+A, B, C = map(int, input().split())
+print((A+B) % C)
+print(((A % C) + (B % C)) % C)
+print((A*B)%C)
+print(((A % C) * (B % C)) % C)
