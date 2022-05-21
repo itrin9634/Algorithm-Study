dcc16ece1d3e7cdd0e04b550e5e74bcdd6ea115c
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-05-21 23:08:40 +0900

    [Silver V] Title: 최대공약수와 최소공배수, Time: 108 ms, Memory: 113248 KB -BaekjoonHub

--- 백준/Silver/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Silver/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,15 +1,6 @@
+import math
+
 a, b = map(int, input().split())
 
-
-def gcd(x, y):
-    while y:
-        x, y = y, x % y
-    return x
-
-
-def lcm(x, y):
-    return x * y // gcd(x, y)
-
-
-print(gcd(a, b))
-print(lcm(a, b))
+print(math.gcd(a,b))
+print(math.lcm(a,b))
