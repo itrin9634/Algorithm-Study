1672f4804e156348f3478bdbc2cf1033039ddee3
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-05-21 23:22:28 +0900

    [Silver III] Title: 소수 구하기, Time: 376 ms, Memory: 123228 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,15 +1,17 @@
+import sys
+input = sys.stdin.readline
+
 m, n = map(int, input().split())
+MAX = 1000000
+dp = [True] * (MAX + 1)
+dp[0], dp[1] = False, False
 
+for i in range(2, MAX+1):
+    j = 2
+    while i*j <= MAX:
+        dp[i*j] = False
+        j += 1
 
-def isPrime(x):
-    if x == 1:
-        return False
-    for i in range(2, int(x ** 0.5) + 1):
-        if x % i == 0:
-            return False
-    return True
-
-
-for i in range(m, n + 1):
-    if isPrime(i):
+for i in range(m, n+1):
+    if dp[i]:
         print(i)
