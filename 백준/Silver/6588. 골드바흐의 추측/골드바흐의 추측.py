132d6de90a7277be0c000b840fcc22baa604efca
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-05-21 23:33:36 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 432 ms, Memory: 123916 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,28 +1,27 @@
-n = int(input())
-a = 0
-b = 0
+import sys
+input = sys.stdin.readline
+MAX = 1000000
+dp = [True] * (MAX + 1)
+dp[0], dp[1] = False, False
+for i in range(2, MAX+1):
+    j = 2
+    while i*j <= MAX:
+        dp[i*j] = False
+        j += 1
 
 
-def isPrime(x):
-    if x == 1:
-        return False
-    for i in range(2, int(x ** 0.5) + 1):
-        if x % i == 0:
-            return False
-    return True
+def goldbach(x):
+    for i in range(3, x, 2):
+        if dp[i] and dp[x-i]:
+            return i
 
 
-def findPrime(x):
-    if x == 1:
-        return False
-    for j in range(3, x, 2):
-        if isPrime(j) and isPrime(x - j):
-            print('{0} = {1} + {2}'.format(x, j, x-j))
-            return j
-
-
-while n != 0:
-    findPrime(n)
+while True:
     n = int(input())
     if n == 0:
         break
+    try:
+        print(str(n) + " = " + str(goldbach(n)) + " + " + str(n - goldbach(n)))
+    except:
+        print("Goldbach's conjecture is wrong.")
+
