57da3ac93d74a0d1afa06ba2f436fe84e52099c1
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-07-11 16:44:05 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,18 +1,11 @@
 def solution(n, arr1, arr2):
     answer = []
-    arr = [[0] * n for _ in range(n)]
-    for i in range(n):
-        arr1[i] = str(bin(arr1[i])[2:])
-        arr1[i] = '0' * (n - len(arr1[i])) + arr1[i]
-        arr2[i] = str(bin(arr2[i])[2:])
-        arr2[i] = '0' * (n - len(arr2[i])) + arr2[i]
-    for i in range(n):
-        tmp = ''
-        for j in range(n):
-            if arr1[i][j] == '0' and arr2[i][j] == '0':
-                tmp += ' '
-            else:
-                tmp += '#'
-        answer.append(tmp) 
+    for i, j in zip(arr1, arr2):
+        a12 = str(bin(i|j)[2:])
+        a12 = a12.rjust(n, '0')
+        a12 = a12.replace('0', ' ')
+        a12= a12.replace('1', '#')
+        answer.append(a12)
+        
     
     return answer
