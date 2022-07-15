45f913f6de996a08fbde7593d24f5e77a669b597
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-07-15 16:00:37 +0900

    [Gold V] Title: 암호 만들기, Time: 132 ms, Memory: 115004 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -0,0 +1,21 @@
+l, c = map(int, input().split())
+alphas = input().split()
+alphas.sort()
+vowels = set(['a', 'e', 'i', 'o', 'u'])
+cons = set([x for x in alphas if x not in vowels])
+tmp = []
+
+
+def sol(idx):
+    if len(tmp) == l and len(set(tmp) & vowels) > 0 and len(set(tmp) & cons) > 1:
+        print(''.join(tmp))
+        return
+
+    for i in range(idx, c):
+        if alphas[i] not in tmp:
+            tmp.append(alphas[i])
+            sol(i)
+            tmp.pop()
+
+
+sol(0)
