6e847d17508bb386a8d0ad5a21ddaf0d3eedfa6d
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-07-16 20:33:19 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1672 ms, Memory: 161376 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+n, m, k, x = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+distance = [-1] * (n+1)
+distance[x] = 0
+q = deque([x])
+while q:
+    now = q.popleft()
+    for next_node in graph[now]:
+        if distance[next_node] == -1:
+            distance[next_node] = distance[now] + 1
+            q.append(next_node)
+check = False
+for i in range(1, n+1):
+    if distance[i] == k:
+        print(i)
+        check = True
+if not check:
+    print(-1)
+
