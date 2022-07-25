b61634837f1b878d2a3d1ca3e1b08d22f03c74ae
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-07-25 20:16:28 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.py
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -1,11 +1,11 @@
 def solution(N, stages):
-    result = {}
-    denominator = len(stages)
-    for stage in range(1, N+1):
-        if denominator != 0:
-            count = stages.count(stage)
-            result[stage] = count / denominator
-            denominator -= count
+    people = len(stages)
+    answer = {}
+    for i in range(1, N+1):
+        if people != 0:
+            cnt = stages.count(i)
+            answer[i] = cnt / people
+            people -= cnt
         else:
-            result[stage] = 0
-    return sorted(result, key=lambda x : result[x], reverse=True)
+            answer[i] = 0
+    return sorted(answer, key = lambda x : answer[x], reverse = True)
