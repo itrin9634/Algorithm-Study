3acd9c9de0629a91402ad2d536ca16f40a3e1ce5
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-07-28 12:51:23 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,30 +1,12 @@
 def solution(id_list, report, k):
-    answer = []
-    dic = {}
-    for i in range(len(id_list)):
-        dic[id_list[i]] = int(i)
-    reversed_dic= dict(map(reversed, dic.items()))
-    reports = [[] * len(id_list) for _ in range(len(id_list))]
-    counts = [0] * len(id_list)
-    
-    # 각 사람마다 신고한 사람 저장
-    for i in range(len(report)):
-        a, b = report[i].split()
-        if b in reports[dic[a]]:
-            continue
-        reports[dic[a]].append(b)
-        counts[dic[b]] += 1
-    
-    # 정지된 사람 목록
-    stop_users = []
-    for i in range(len(counts)):
-        if counts[i] >= k:
-            stop_users.append(reversed_dic[i])
-    
-    for i in range(len(reports)):
-        cnt = 0
-        for j in range(len(stop_users)):
-            if stop_users[j] in reports[i]:
-                cnt += 1
-        answer.append(cnt)
+    answer = [0] * len(id_list)    
+    reports = {x : 0 for x in id_list}
+
+    for r in set(report):
+        reports[r.split()[1]] += 1
+
+    for r in set(report):
+        if reports[r.split()[1]] >= k:
+            answer[id_list.index(r.split()[0])] += 1
+
     return answer
