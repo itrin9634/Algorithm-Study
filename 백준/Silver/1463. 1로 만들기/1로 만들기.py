80f6b084a19419a7c78c23a8af8b32ab65021acb
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-07-30 18:17:00 +0900

    [Silver III] Title: 1로 만들기, Time: 140 ms, Memory: 122304 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,11 +1,11 @@
 n = int(input())
-count = 0
-d = [0] * (10**6 + 1)
+dp = [int(1e9)] * (n+1)
+dp[1] = 0
 for i in range(2, n+1):
-    d[i] = d[i-1] + 1
-
+    dp[i] = dp[i-1] + 1
     if i % 3 == 0:
-        d[i] = min(d[i], d[i//3]+1)
+        dp[i] = min(dp[i], dp[i // 3] + 1)
     if i % 2 == 0:
-        d[i] = min(d[i], d[i//2]+1)
-print(d[n])
+        dp[i] = min(dp[i], dp[i // 2] + 1)
+print(dp[n])
+
