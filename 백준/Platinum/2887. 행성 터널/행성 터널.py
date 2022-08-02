65c301dddcc8ba71089b22cad9d4b3fad2843368
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-08-02 15:22:28 +0900

    [Platinum V] Title: 행성 터널, Time: 2208 ms, Memory: 183920 KB -BaekjoonHub

---
+++ 백준/Platinum/2887. 행성 터널/행성 터널.py
@@ -0,0 +1,46 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+n = int(input())
+parent = [i for i in range(n+1)]
+edges = []
+x = []
+y = []
+z = []
+for i in range(1, n+1):
+    data = list(map(int, input().split()))
+    x.append((data[0], i))
+    y.append((data[1], i))
+    z.append((data[2], i))
+
+x.sort()
+y.sort()
+z.sort()
+
+for i in range(n-1):
+    edges.append((x[i + 1][0] - x[i][0], x[i][1], x[i + 1][1]))
+    edges.append((y[i + 1][0] - y[i][0], y[i][1], y[i + 1][1]))
+    edges.append((z[i + 1][0] - z[i][0], z[i][1], z[i + 1][1]))
+
+edges.sort()
+result = 0
+
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+print(result)
