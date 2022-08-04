942e3ee7b743963b7602a584b1ecbaa3aa509502
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-08-04 21:35:29 +0900

    [level 2] Title: 2 x n 타일링, Time: 7.22 ms, Memory: 11.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -0,0 +1,8 @@
+def solution(n):
+    mod = 1000000007
+    dp = [0] * (n+1)
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, n+1):
+        dp[i] = (dp[i-1] + dp[i-2]) % mod
+    return (dp[n] % mod)
