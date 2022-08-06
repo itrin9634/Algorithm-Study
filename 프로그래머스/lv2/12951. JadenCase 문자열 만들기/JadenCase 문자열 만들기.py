ecc19786c41f378440f18b584f0ec1cac1dc5a71
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-08-06 13:30:22 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,16 @@
+def solution(s):
+    s = s.lower()
+    idx = 0
+    answer = ''
+    for i in range(len(s)):
+        if idx == 0 and s[i].isalpha():
+            answer += s[i].upper()
+            idx += 1
+            continue
+        elif s[i] == ' ':
+            idx = 0
+        else:
+            idx += 1
+        answer += s[i]
+        
+    return answer
