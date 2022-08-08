2d0dce49fd990acaa61ed98944757a0449ef3432
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-08-08 14:35:12 +0900

    [Gold IV] Title: 이분 그래프, Time: 1696 ms, Memory: 216288 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,47 +1,41 @@
 from collections import deque
 
-k = int(input())
+
+def bfs(start, group):
+    q = deque()
+    q.append(start)
+    global error
+    visited[start] = group
+
+    while q:
+        now = q.popleft()
+        for i in graph[now]:
+            if not visited[i]:
+                visited[i] = -(visited[now])
+                q.append(i)
+            elif visited[i] == visited[now]:
+                error = True
+                return
 
 
-def bfs(graph, start):
-    queue = deque()
-    queue.append(start)
-    if visited[start] == 0:
-        visited[start] = 1  # 이분은 1, 2로 하고 아직 방문하지 않은곳은 0으로 표시
-    while queue:
-        v = queue.popleft()
+t = int(input())
+for _ in range(t):
+    v, e = map(int, input().split())
+    graph = [[] for _ in range(v+1)]
+    visited = [False] * (v+1)
+    error = False
 
-        color = visited[v]
-        for i in graph[v]:  # V정점과 연결된 모든 정점 확인
-            if visited[i] == 0:  # 아직 한번도 방문하지 않음
-                queue.append(i)  # 다음에 방문할 곳으로 지정
-                if color == 1:  # 현재의 정점과 다른 색상으로 색칠
-                    visited[i] = 2
-                else:
-                    visited[i] = 1
-            elif visited[i] == 1:
-                if color == 1:
-                    print("NO")
-                    return False
-            elif visited[i] == 2:
-                if color == 2:
-                    print("NO")
-                    return False
-    return True
-
-
-for i in range(k):
-    flag = 0
-    V, E = map(int, input().split())
-    graph = [[] for _ in range(V + 1)]
-    visited = [0] * (V + 1)
-    for j in range(E):
+    for _ in range(e):
         a, b = map(int, input().split())
         graph[a].append(b)
         graph[b].append(a)
-    for k in range(1, V + 1): # 연결그래프일 경우에는 시작점에서 한번의 BFS를 수행하면 되지만 비연결그래프의 경우에는 모든 정점을 탐색해야 한다.
-        if not bfs(graph, k):
-            flag = 1
+
+    for i in range(1, v+1):
+        if not visited[i]:
+            bfs(i, 1)
+        if error:
             break
-    if flag == 0:
-        print("YES")
+    if error:
+        print('NO')
+    else:
+        print('YES')
