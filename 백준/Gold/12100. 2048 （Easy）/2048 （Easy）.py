6ccbdce721fec4f8f933da6c5fce0cc09d383875
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-08-08 19:44:45 +0900

    [Gold II] Title: 2048 (Easy), Time: 300 ms, Memory: 117824 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
@@ -1,96 +1,96 @@
-import sys
 from copy import deepcopy
+import sys
 input = sys.stdin.readline
 
-# INPUT
-n = int(input())
-board = [list(map(int, input().split())) for _ in range(n)]
-answer = 0
 
-# UP
-def up(board):
+def up(arr):
     for j in range(n):
-        pointer = 0
+        p = 0
         for i in range(1, n):
-            if board[i][j]:
-                tmp = board[i][j]
-                board[i][j] = 0
-                # 포인터가 가리키는 수가 0일 때
-                if board[pointer][j] == 0:
-                    board[pointer][j] = tmp
-                # 포인터가 가리키는 수와 현재 위치의 수가 같을 때
-                elif board[pointer][j]  == tmp:
-                    board[pointer][j] *= 2
-                    pointer += 1
-                # 포인터가 가리키는 수와 현재 위치의 수가 다를 때
+            if arr[i][j]:
+                tmp = arr[i][j]
+                arr[i][j] = 0
+                if arr[p][j] == 0:
+                    arr[p][j] = tmp
+                elif arr[p][j] == tmp:
+                    arr[p][j] *= 2
+                    p += 1
                 else:
-                    pointer += 1
-                    board[pointer][j] = tmp
-    return board
+                    p += 1
+                    arr[p][j] = tmp
+    return arr
 
-# DOWN
-def down(board):
+
+def down(arr):
     for j in range(n):
-        pointer = n - 1
-        for i in range(n - 2, -1, -1):
-            if board[i][j]:
-                tmp = board[i][j]
-                board[i][j] = 0
-                if board[pointer][j] == 0:
-                    board[pointer][j] = tmp
-                elif board[pointer][j]  == tmp:
-                    board[pointer][j] *= 2
-                    pointer -= 1
+        p = n-1
+        for i in range(n-2, -1, -1):
+            if arr[i][j]:
+                tmp = arr[i][j]
+                arr[i][j] = 0
+                if arr[p][j] == 0:
+                    arr[p][j] = tmp
+                elif arr[p][j] == tmp:
+                    arr[p][j] *= 2
+                    p -= 1
                 else:
-                    pointer -= 1
-                    board[pointer][j] = tmp
-    return board
+                    p -= 1
+                    arr[p][j] = tmp
+    return arr
 
-# LEFT
-def left(board):
+
+def left(arr):
     for i in range(n):
-        pointer = 0
+        p = 0
         for j in range(1, n):
-            if board[i][j]:
-                tmp = board[i][j]
-                board[i][j] = 0
-                if board[i][pointer] == 0:
-                    board[i][pointer] = tmp
-                elif board[i][pointer]  == tmp:
-                    board[i][pointer] *= 2
-                    pointer += 1
+            if arr[i][j]:
+                tmp = arr[i][j]
+                arr[i][j] = 0
+                if arr[i][p] == 0:
+                    arr[i][p] = tmp
+                elif arr[i][p] == tmp:
+                    arr[i][p] *= 2
+                    p += 1
                 else:
-                    pointer += 1
-                    board[i][pointer]= tmp
-    return board
+                    p += 1
+                    arr[i][p] = tmp
+    return arr
 
-# RIGHT
-def right(board):
+
+def right(arr):
     for i in range(n):
-        pointer = n - 1
-        for j in range(n - 2, -1, -1):
-            if board[i][j]:
-                tmp = board[i][j]
-                board[i][j] = 0
-                if board[i][pointer] == 0:
-                    board[i][pointer] = tmp
-                elif board[i][pointer]  == tmp:
-                    board[i][pointer] *= 2
-                    pointer -= 1
+        p = n-1
+        for j in range(n-2, -1, -1):
+            if arr[i][j]:
+                tmp = arr[i][j]
+                arr[i][j] = 0
+                if arr[i][p] == 0:
+                    arr[i][p] = tmp
+                elif arr[i][p] == tmp:
+                    arr[i][p] *= 2
+                    p -= 1
                 else:
-                    pointer -= 1
-                    board[i][pointer] = tmp
-    return board
+                    p -= 1
+                    arr[i][p] = tmp
+    return arr
 
 
-# DFS
 def dfs(board, cnt):
+    global ans
     if cnt == 5:
-        # 2차원 배열 요소 중 가장 큰 값 반환
-        return max(map(max, board))
+        for i in range(n):
+            for j in range(n):
+                ans = max(ans, board[i][j])
+        return
 
-    # 상, 하, 좌, 우로 움직여 리턴한 값들 중 가장 큰 값 반환
-    # board를 꼭 deepcopy하여 함수를 거친 board값이 다음 함수에 들어가지 못하도록 해주어야 한다.
-    return max(dfs(up(deepcopy(board)), cnt + 1), dfs(down(deepcopy(board)), cnt + 1), dfs(left(deepcopy(board)), cnt + 1), dfs(right(deepcopy(board)), cnt + 1))
+    dfs(up(deepcopy(board)), cnt + 1)
+    dfs(down(deepcopy(board)), cnt + 1)
+    dfs(left(deepcopy(board)), cnt + 1)
+    dfs(right(deepcopy(board)), cnt + 1)
 
-print(dfs(board, 0))
+
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
+ans = 0
+dfs(graph, 0)
+print(ans)
