526bcb1dc92ca39f1411df56579f44f40bbf30d8
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-08-09 16:33:43 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 784 ms, Memory: 116844 KB -BaekjoonHub

---
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.py
@@ -0,0 +1,40 @@
+import sys
+input = sys.stdin.readline
+
+n,m,r = map(int,input().split())
+
+data = [list(map(int,input().split())) for _ in range(n)]
+
+for _ in range(r):
+    for i in range(min(n, m) // 2):
+        x, y = i, i
+        temp = data[x][y]
+        
+        for j in range(i + 1, n - i):  #좌
+            x = j
+            prev_value = data[x][y]
+            data[x][y] = temp
+            temp = prev_value
+
+        for j in range(i + 1, m - i):  #하
+            y = j
+            prev_value = data[x][y]
+            data[x][y] = temp
+            temp = prev_value
+
+        for j in range(i + 1, n - i):  #우
+            x = n - j - 1
+            prev_value = data[x][y]
+            data[x][y] = temp
+            temp = prev_value
+
+        for j in range(i + 1, m - i):  #상
+            y = m - j -1
+            prev_value = data[x][y]
+            data[x][y] = temp
+            temp = prev_value
+
+for i in range(n):
+    for j in range(m):
+        print(data[i][j], end=' ')
+    print()
