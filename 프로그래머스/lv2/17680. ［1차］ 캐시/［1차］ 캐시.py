82ecdd1c8a0e09fc1bf195fa1fa5995be3d61f6d
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-09-05 17:35:18 +0900

    [level 2] Title: [1차] 캐시, Time: 0.65 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+def solution(cacheSize, cities):
+    answer = 0
+    cache = deque()
+    for city in cities:
+        city = city.lower()
+        if cacheSize:
+            if city not in cache:
+                if len(cache) == cacheSize:
+                    cache.popleft()
+                cache.append(city)
+                answer += 5
+            else:
+                cache.remove(city)
+                cache.append(city)
+                answer += 1
+        else:
+            answer += 5
+    
+    return answer
