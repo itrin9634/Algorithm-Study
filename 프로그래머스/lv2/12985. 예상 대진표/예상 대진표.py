c870c65ae80df9ae1608b44bfa7f61f4129c993b
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-09-07 17:46:46 +0900

    [level 2] Title: 예상 대진표, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
@@ -1,22 +1,9 @@
-def is_meet(n, a, b):
-    meet = False
-    if 0 < a <= n and 0 < b <= n and b - a == 1:
-        if a % 2 == 1 and b % 2 == 0:
-            meet = True
-    return meet
-
-
 def solution(n,a,b):
     answer = 0
-    if a > b:
-        a, b = b, a
     
-    while n > 1:
+    while a != b:
         answer += 1
-        if is_meet(n, a, b):
-            break
         a = (a + 1) // 2
         b = (b + 1) // 2
-        n //= 2
 
     return answer
