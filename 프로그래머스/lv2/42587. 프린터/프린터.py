37e0366d91ad1ea879f10def31fe9dcde644e776
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-09-10 19:33:21 +0900

    [level 2] Title: 프린터, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/프린터.py
+++ 프로그래머스/lv2/42587. 프린터/프린터.py
@@ -1,21 +1,18 @@
 from collections import deque
+
 def solution(priorities, location):
     answer = 0
-    hash = {}
-    q = deque()
-    for i in range(len(priorities)):
-        hash[i] = priorities[i]
-        q.append((priorities[i], i))
-    print(q)
-    print(hash)
-    cnt = 0
+    
+    arr = [[i, v] for v, i in enumerate(priorities)]
+    q = deque(arr)
+    max_val = max(q)[0]
     while q:
-        score, idx = q.popleft()
-        MAX = max(hash.values())
-        if MAX > score:
-            q.append((score, idx))
+        max_val = max(q)[0]
+        val, idx = q.popleft()
+        if val != max_val:
+            q.append([val, idx])
         else:
-            cnt += 1
-            del hash[idx]
-            if location == idx:
-                return cnt
+            if idx == location:
+                return answer + 1
+            else:
+                answer += 1
