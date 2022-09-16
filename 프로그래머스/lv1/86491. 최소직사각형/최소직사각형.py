eff6fbcaa0fd2a4849cbb6a1be4490e596b9a4e5
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-09-16 13:08:15 +0900

    [level 1] Title: 최소직사각형, Time: 3.18 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
@@ -1,7 +1,7 @@
 def solution(sizes):
     answer = 0
 
-    arr1 = [min(v, i) for (i, v) in sizes]
+    arr1 = [min(i, v) for (i, v) in sizes]
     arr2 = [max(i, v) for (i, v) in sizes]
     answer = max(arr1) * max(arr2)
     return answer
