759ccc1eec1c3a078778e9cd78c2ce9664d738e4
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-09-25 14:00:48 +0900

    [Silver I] Title: 트리 순회, Time: 108 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,32 @@
+n = int(input())
+tree = {}
+for i in range(n):
+    a, b, c = input().split()
+    tree[a] = [b, c]
+
+def preorder(root):
+    if root != '.':
+        print(root, end = '')
+        preorder(tree[root][0])
+        preorder(tree[root][1])
+
+
+def inorder(root):
+    if root != '.':
+        inorder(tree[root][0])
+        print(root, end = '')
+        inorder(tree[root][1])
+
+def postorder(root):
+    if root != '.':
+        postorder(tree[root][0])
+        postorder(tree[root][1])
+        print(root, end = '')
+
+
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
+print()
