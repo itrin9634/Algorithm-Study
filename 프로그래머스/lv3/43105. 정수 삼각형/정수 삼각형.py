83e560adaf39e107ab9804215ab0269bcfe2fd8e
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-09-27 03:41:19 +0900

    [level 3] Title: 정수 삼각형, Time: 46.34 ms, Memory: 14.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,11 @@
+def solution(triangle):
+    answer = 0
+    for i in range(1, len(triangle)):
+        for j in range(len(triangle[i])):
+            if j == 0:
+                triangle[i][j] += triangle[i-1][j]
+            elif j == len(triangle[i]) - 1:
+                triangle[i][j] += triangle[i-1][j-1]
+            else:
+                triangle[i][j] += max(triangle[i-1][j], triangle[i-1][j-1])
+    return max(triangle[-1])
