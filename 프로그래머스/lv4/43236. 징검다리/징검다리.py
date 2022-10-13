deae8f9e59053eddf7331efdac58382471389f59
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-10-14 02:12:23 +0900

    [level 4] Title: 징검다리, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv4/43236. 징검다리/징검다리.py
+++ 프로그래머스/lv4/43236. 징검다리/징검다리.py
@@ -1,23 +1,10 @@
 def solution(distance, rocks, n):
     answer = 0
-    start, end = 0, distance
     rocks.sort()
-    
-    while start  <= end:
-        cnt = 0
-        mid = (start + end) // 2
-        pre = 0
-        for i in rocks:
-            if i - pre < mid:
-                cnt += 1
-            else:
-                pre = i
-                
-        if cnt > n:
-            end = mid - 1
-        else:
-            answer = mid
-            start = mid + 1
-        
-        
+    dists = [rocks[0]]
+    before = 0
+    for i in range(1, len(rocks)):
+        dists.append(abs(rocks[i] - rocks[i-1]))
+    dists.append(abs(rocks[-1] - distance))
+    print(dists)
     return answer
