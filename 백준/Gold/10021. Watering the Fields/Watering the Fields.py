ba96875aba0ac5f8842d28530a19a0e5edfd6edc
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-10-15 21:33:01 +0900

    [Gold III] Title: Watering the Fields, Time: 2936 ms, Memory: 368108 KB -BaekjoonHub

--- 백준/Gold/10021. Watering the Fields/Watering the Fields.py
+++ 백준/Gold/10021. Watering the Fields/Watering the Fields.py
@@ -1,41 +1,54 @@
 import sys
 import heapq
+input = sys.stdin.readline
 
-n, base_cost = map(int, sys.stdin.readline().rstrip().split())
-nodes = []
-for _ in range(n): nodes.append(list(map(int, sys.stdin.readline().rstrip().split())))
 
-parents = [i for i in range(n)]
-pq = []
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
 
-for i in range(n):
-    for j in range(i+1, n):
-        a, b = nodes[i]
-        c, d = nodes[j]
-        cost = abs(a-c)**2 + abs(b-d)**2
-        if cost >= base_cost: heapq.heappush(pq, [cost, i, j])
 
-def find(node):
-    if parents[node] == node: return node
+def union(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a < b:
+        parent[b] = a
     else:
-        parents[node] = find(parents[node])
-        return parents[node]
-def union(node1, node2):
-    root1, root2 = find(node1), find(node2)
-    if root1 == root2: return False
+        parent[a] = b
+
+
+ans, cnt = 0, 0
+n, c = map(int, input().split())
+parent = [i for i in range(n)]
+points = []
+for _ in range(n):
+    x, y = map(int, input().split())
+    points.append((x, y))
+
+h = []
+for i in range(len(points)-1):
+    for j in range(i+1, len(points)):
+        x1, y1 = points[i]
+        x2, y2 = points[j]
+        dist = (x1 - x2) ** 2 + (y1 - y2) ** 2
+        if dist >= c:
+            heapq.heappush(h, (dist, i, j))
+
+if len(h) < n - 1:
+    print(-1)
+else:
+    while h:
+        d, p1, p2 = heapq.heappop(h)
+        if find_parent(parent, p1) != find_parent(parent, p2):
+            union(parent, p1, p2)
+            cnt += 1
+            ans += d
+            if cnt == n - 1:
+                break
+    if cnt == n - 1:
+        print(ans)
     else:
-        parents[root2] = root1
-        return True
+        print(-1)
 
-total = 0
-edge_num = 0
-while pq:
-    cur_cost, node1, node2 = heapq.heappop(pq)
-
-    if union(node1, node2):
-        total += cur_cost
-        edge_num += 1
-        if edge_num == n-1: break
-
-if edge_num == n-1: print(total)
-else: print(-1)
