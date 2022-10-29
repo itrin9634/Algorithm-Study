4cff2c91f15ae02ee49d50f5b6a5e54d61def6b5
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2022-10-30 03:08:58 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
@@ -3,16 +3,10 @@ def solution(ingredient):
     stack = []
     for i in range(len(ingredient)):
         stack.append(ingredient[i])
-        if len(stack) >= 4:
-            f4 = stack.pop()
-            f3 = stack.pop()
-            f2 = stack.pop()
-            f1 = stack.pop()
-            if f1 == 1 and f2 == 2 and f3 == 3 and f4 == 1:
-                answer += 1
-            else:
-                stack.append(f1)
-                stack.append(f2)
-                stack.append(f3)
-                stack.append(f4)
+        if len(stack) >= 4 and stack[-4:] == [1, 2, 3, 1]:
+            answer += 1
+            stack.pop()
+            stack.pop()
+            stack.pop()
+            stack.pop()
     return answer
