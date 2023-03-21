b567657e7908c0ebafa81324787a3df6739c6949
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2023-03-21 17:26:29 +0900

    [Gold V] Title: 전구와 스위치, Time: 148 ms, Memory: 123716 KB -BaekjoonHub

--- 백준/Gold/2138. 전구와 스위치/전구와 스위치.py
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.py
@@ -1,42 +1,45 @@
-from sys import stdin
-import copy
-
 n = int(input())
-A = list(map(int, stdin.readline().rstrip()))
-B = list(map(int, stdin.readline().rstrip()))
+state = list(map(int, input()))
+goal = list(map(int, input()))
 
-r1 = copy.deepcopy(A)
-r2 = copy.deepcopy(A)
+def zeroClick(state):
+    cnt = 1
+    state[0] = 1 - state[0]
+    state[1] = 1 - state[1]
 
-def two_flip(i, j):
-    A[i] = 1 - A[i]
-    A[j] = 1 - A[j]
+    for i in range(1, n):
+        if state[i-1] != goal[i-1]:
+            cnt += 1
+            state[i-1] = 1- state[i-1]
+            state[i] = 1 - state[i]
+            if i != (n - 1):
+                state[i+1] = 1 - state[i+1]
+    if state == goal:
+        return cnt
+    else:
+        return -1
 
-
-def three_flip(i, j, k):
-    A[i] = 1 - A[i]
-    A[j] = 1 - A[j]
-    A[k] = 1 - A[k]
-
-for i in range(2):
-    A = r1 if i == 0 else r2
-
+def zeroNoClick(state):
     cnt = 0
-    for j in range(n):
-        if j == 0:
-            if i == 0 and A != B:
-                cnt += 1
-                two_flip(j, j+1)
-        elif 1 <= j <= n-2:
-            if A[j-1] != B[j-1]:
-                cnt += 1
-                three_flip(j-1, j, j+1)
-        elif j == n-1:
-            if A[j-1] != B[j-1]:
-                cnt += 1
-                two_flip(j-1, j)
-    if A == B:
-        print(cnt)
-        break
-if A != B:
+    for i in range(1, n):
+        if state[i-1] != goal[i-1]:
+            cnt += 1
+            state[i-1] = 1- state[i-1]
+            state[i] = 1 - state[i]
+            if i != (n - 1):
+                state[i+1] = 1 - state[i+1]
+    if state == goal:
+        return cnt
+    else:
+        return -1
+
+res1 = zeroClick(state[:])
+res2 = zeroNoClick(state[:])
+if res1 >= 0 and res2 >= 0:
+    print(min(res1, res2))
+elif res1 >= 0 and res2 < 0:
+    print(res1)
+elif res1 < 0 and res2 >= 0:
+    print(res2)
+else:
     print(-1)
