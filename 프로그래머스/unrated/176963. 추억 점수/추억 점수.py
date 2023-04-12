c93e18f425498abb9d9fe4b51d40b56512b8bd1f
Hyerin Park <48737861+itrin9634@users.noreply.github.com>
2023-04-12 14:24:51 +0900

    [unrated] Title: 추억 점수, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+def solution(name, yearning, photo):
+    answer = []
+    n_dict = defaultdict(int)
+    for i in range(len(name)):
+        n_dict[name[i]] = yearning[i]
+        
+    for i in range(len(photo)):
+        total = 0
+        for j in range(len(photo[i])):
+            total += n_dict[photo[i][j]]
+            print(total)
+        answer.append(total)
+        
+        
+    return answer
